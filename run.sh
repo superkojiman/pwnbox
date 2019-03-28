25b9436f21e639e1de0d6ff6b701a165b2c866e0
Margular <2609135351@qq.com>
2019-03-28 20:28:11 +0800

    interactive

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 # Create docker container and run in the background
 # Add this if you need to modify anything in /proc:  --privileged 
-docker run -it -h pwnbox -d -v $HOME/ctf:/root/ctf --security-opt seccomp:unconfined --name pwnbox pwnbox /bin/bash
+docker run -it -h pwnbox -d -v $HOME/ctf:/root/ctf --security-opt seccomp:unconfined --name pwnbox superkojiman/pwnbox
+docker exec -it pwnbox /bin/bash
